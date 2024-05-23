0037aa4ef9077d1253d5244a7b15e136d9da38c0
salasvictor22 <jsalasag@unsa.edu.pe>
2024-05-22 19:40:31 -0500

    ejercicio2a.py resuelto

--- Ejercicio2a.py
+++ Ejercicio2a.py
@@ -2,12 +2,12 @@ from interpreter import draw
 from pieces import *
 from picture import *
 
-
 knight = Picture(KNIGHT)
 knight_Black = Picture(KNIGHT)
 knight_Black.negative()
-
 combined_knights = knight.join(knight_Black)
-draw(combined_knights.img)
+combined_knights2 = knight_Black.join(knight)
+image = combined_knights.up(combined_knights2)
+draw(image.img)
 
 
